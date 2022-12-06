33700c57ac4cec2e96ad32ea447f88000e913079
anankebj <anankebj@gmail.com>
2022-12-05 19:09:29 -0600

    adding mockData

--- src/mockData.js
+++ src/mockData.js
@@ -1,17 +1,85 @@
 const Menus = {
   iceCreamMenu: [
-    'Greek Yogurt + Toppings',
-    'Taro + Toppings',
-    'Matcha + Toppings',
-    'Hazelnut Chocolate + Toppings',
-    'Strawberry + Toppings',
-    `Vanilla McDonald's + Toppings`,
+    {
+      id: 'greek yogurtic',
+      name: 'Greek Yogurt + Toppings',
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'taroic',
+      name: 'Taro + Toppings',
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'matchaic',
+      name: 'Matcha + Toppings',
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'hazelnutchocolateic',
+      name: 'Hazelnut Chocolate + Toppings',
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'strawberryic',
+      name: 'Strawberry + Toppings',
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'vanillaic',
+      name: `Vanilla McDonald's + Toppings`,
+      price: 65,
+      type: 'ice cream',
+    },
+    {
+      id: 'milkshake',
+      name: `MilkShake`,
+      price: 75,
+      type: 'milkshake',
+    },
+    {
+      id: 'ltic',
+      name: `Liter`,
+      price: 200,
+      type: 'ice cream',
+    },
   ],
   snacks: [
-    'Chocolate Chips Cookie',
-    'Taro cream Filled Wafers',
-    'Spirulina Filled Wafers',
-    'Sugar Free Chocolate Mamut',
+    {
+      id: 'chocolatecookie',
+      name: `Chocolate Chips Cookie`,
+      price: 35,
+      type: 'snack',
+    },
+    {
+      id: 'tarowaffer',
+      name: 'Taro cream Filled Wafers',
+      price: 25,
+      type: 'snack',
+    },
+    {
+      id: 'tarowaffer',
+      name: 'Taro cream Filled Wafers',
+      price: 25,
+      type: 'snack',
+    },
+    {
+      id: 'spirulinawaffer',
+      name: 'Spirulina Filled Wafers',
+      price: 25,
+      type: 'snack',
+    },
+    {
+      id: 'mamut',
+      name: 'Sugar Free Chocolate Mamut',
+      price: 50,
+      type: 'snack',
+    },
   ],
 };
 
